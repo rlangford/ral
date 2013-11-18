e2ab7a974193ce9493b507f5b3d2ad637fdb4031
rlangford <rlangford@gmail.com>
2013-11-18 01:36:29 -0600

    minor updatezz

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var app = express();
 app.set('port', process.env.PORT || 1338);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-//app.use(express.favicon());
 app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')))
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
